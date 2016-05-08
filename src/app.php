e5da3b9266b09a2e20788357ab7675f5769b3108
Marcos Segovia <velozmarkdrea@gmail.com>
2016-05-08 21:35:55 +0200

    Setting Redis provider service.

--- src/app.php
+++ src/app.php
@@ -17,5 +17,21 @@ $app->register(new SessionServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new DoctrineServiceProvider);
 $app->register(new DoctrineOrmServiceProvider);
+$app->register(new Predis\Silex\ClientsServiceProvider(), [
+        'predis.clients' => [
+            'redis-master' => [
+                'host' => '127.0.0.1',
+                'port' => 6379
+            ],
+            'redis-slave1' => [
+                'host' => '',
+                'port' => 6379,
+            ],
+            'redis-slave2' => [
+                'host' => '',
+                'port' => 6379
+            ],
+        ]
+]);
 
 return $app;
